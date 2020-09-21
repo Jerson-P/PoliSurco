b997cc693bbcda77db52d1ce483df3f7c4876cef
crojastriana3 <cristianrojas@unimayor.edu.co>
2020-09-21 15:26:58 -0500

    Emisora y navegador

---
+++ src/app/shared/emisora/emisora.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emisora',
+  templateUrl: './emisora.component.html',
+  styleUrls: ['./emisora.component.css']
+})
+export class EmisoraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
